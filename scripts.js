38a1061ccb687100c2f7cc97d982b0e6fd4b0695
yoannyalvarez <imasmilergirl@hotmail.com>
2021-02-06 21:16:46 -0500

    xfbfxg

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 function converter() {
-    // INPUT: 
+    // INPUT: Gets the value of the weight in pounds that the user has inputed.
     let pounds = parseFloat(document.getElementById('pounds').value);
-    // PROCESSING:
+    // PROCESSING: Creates a variales called kilograms that holds the value of the input multipliyed by his value per kilograms that the weight has in pounds.
     let kilograms = pounds * 0.453592;
-    // OUTPUT: 
+    // OUTPUT: Display the value of the weight in kilograms with its respective kilograms sign. 
     document.getElementById('output').innerHTML = kilograms.toFixed(1) + " kg";
 }
