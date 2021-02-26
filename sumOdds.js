a444a9a5e6b4ab5de32fe273337b6c2e00faa719
yoannyalvarez <imasmilergirl@hotmail.com>
2021-02-26 17:51:19 -0500

    gg

--- sumOdds.js
+++ sumOdds.js
@@ -1,19 +1,18 @@
 function sum() {
-    // INPUT: Getting the subtotal, that the user has entered and store it in a variable and using .getDay to get current day.
+    // INPUT: Getting the ceiling number that the user has entered and store it in a variable.
     let n = parseInt(document.getElementById('number').value);
-    let sum = 0;
+    
     /* 
     PROCESSING:
-    Declaring a variable that holds the total amount
-    Checking day and amount to calculate totalamount:
-        if subtotal is greater than $50 and is Tuesday or Wednesday:
-            totalamount will be the subtotal - discount + tax
-        if not: totalamount will be the subtotal + tax
-    */ 
+    Declaring a variable that holds the total sum.
+    Creating a loop that iterates numbers between 1 and ceiling number by two.
+    Adding the value of every iteration variable to the sum variable.    
+    */
+    let sum = 0; 
     for (let i = 1; i <= n; i += 2) {
         sum += i;
     }
 
-    // OUTPUT: Storing the totalamount value  to the output div
+    // OUTPUT: Storing the sum value to the output div
     document.getElementById('output').innerHTML = "The sum is: " + sum; 
 }
