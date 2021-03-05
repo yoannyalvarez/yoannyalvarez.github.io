0be0b9b1b8a10580b51a7193406e5a423d49199c
yoannyalvarez <imasmilergirl@hotmail.com>
2021-03-05 17:13:36 -0500

    sefesf

--- fiveTimes.js
+++ fiveTimes.js
@@ -1,13 +1,13 @@
 function display() {
-    // INPUT: Getting the subtotal, that the user has entered and store it in a variable and using .getDay to get current day.
+    // INPUT: No input
     
     /* 
     PROCESSING:
-    Declaring a variable that holds the total amount
-    Checking day and amount to calculate totalamount:
-        if subtotal is greater than $50 and is Tuesday or Wednesday:
-            totalamount will be the subtotal - discount + tax
-        if not: totalamount will be the subtotal + tax
+    Declaring a variable that holds the result of multiplication amount
+    Declaring a variable that holds the output statement
+    Creating a loop of 12 iterations of i. 
+        calculating the result
+        storing the new output statement
     */ 
     let result;
     let output = "";
@@ -17,6 +17,6 @@ function display() {
     }
 
 
-   // OUTPUT: Storing the totalamount value  to the output div
+   // OUTPUT: Storing the final output statement to the output div
    document.getElementById('output').innerHTML = output; 
 }
